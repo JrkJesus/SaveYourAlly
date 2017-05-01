11a7bd1eb039f306da60b28fc3c42e6a91cb796e
JrkJesus <jesus21hlv@gmail.com>
2017-05-01 19:00:00 +0200

    unsave changes

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
+    public float timeMax = 60.0f;
+    public float countDown;
 
 	// Use this for initialization
 	void Start () {
-		
+        countDown = timeMax;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        countDown -= Time.deltaTime;
+        if( countDown <= 0)
+        {
+            // ToDo: Reset
+             // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 	}
 }
