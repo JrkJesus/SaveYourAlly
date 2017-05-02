77ffad533a715a281b25ccd7daa53da051fcf79f
Antonio Ruz <ruzperezantonio@gmail.com>
2017-05-02 16:52:10 +0200

    Añadida textura del personaje

---
+++ Assets/Script/PhaseChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhaseChange : MonoBehaviour {
+    public int level=0;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("portalCambioNivel"))
+        {
+            SceneManager.LoadScene("level" + level);
+
+        }
+    }
+}
