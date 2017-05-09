f29c56d29d1d6aecc042b3ae1d90f3da1e8a9f72
juanfranciscoviejo <juanfranciscova@hotmail.com>
2017-05-09 16:15:40 +0200

    Añadidos scripts para coger cajas de municion y salud. Añadidos prefabs de las cajas

---
+++ Assets/Script/PickHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickHealth : MonoBehaviour {
+
+
+    public int nhealth = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Caja salud soltada");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Entra");
+        if (collider.CompareTag("Player"))
+        {
+            Health health = collider.GetComponent<Health>();
+            Debug.Log("Caja: " + nhealth);
+            health.healthPoints += nhealth;
+            Destroy(gameObject);
+        }
+    }
+}
