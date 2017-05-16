05624a351f8c785bac07454f21cac25f531d3ab0
AntonioRuzPerez <ruzperezantonio@gmail.com>
2017-05-16 15:06:49 +0200

    Creados portales de nuevo

--- Assets/Script/RotatingItem.cs
+++ Assets/Script/RotatingItem.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotatingItem : MonoBehaviour {
 
+    public float rotaY = 0;
+    public float rotaX = 0;
+    public float rotaZ = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +15,6 @@ public class RotatingItem : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
        // transform.Rotate(new Vector3(0, 0, Time.deltaTime * 80)); //eje z
-        transform.Rotate(new Vector3(0, Time.deltaTime * 80, 0)); //eje y
+        transform.Rotate(new Vector3(Time.deltaTime * rotaX, Time.deltaTime * rotaY, Time.deltaTime * rotaZ)); //eje y
 	}
 }
